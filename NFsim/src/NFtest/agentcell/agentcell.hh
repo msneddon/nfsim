604c582d57aff1819888d75a925df1836e2e214d
msneddon <mwsneddon@lbl.gov>
2009-01-30 22:09:41 +0000

    Files needed to run an agentCell model.

---
+++ NFsim/src/NFtest/agentcell/agentcell.hh
@@ -0,0 +1,18 @@
+/*
+ * agentcell.hh
+ *
+ *  Created on: Jan 20, 2009
+ *      Author: msneddon
+ */
+
+#ifndef AGENTCELL_HH_
+#define AGENTCELL_HH_
+
+#include <string>
+#include <map>
+
+using namespace std;
+void runAgentCell(map<string,string> argMap, bool verbose);
+
+
+#endif /* AGENTCELL_HH_ */
