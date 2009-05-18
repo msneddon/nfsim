c6a75f3d9b533435b7dc2edd5503bb6fcb4f3146
kstanton <kstanton@e73d0046-41a0-3a10-3dbd-efb5796e4b37>
2009-05-18 19:45:34 +0000

    Minor tweaks.  Check in of working copy before adding interaction class functionality

---
+++ src/NFreactions/Compartment/CompartmentInteraction.h
@@ -0,0 +1,17 @@
+/*
+ * CompartmentInteraction.h
+ *
+ *  Created on: May 18, 2009
+ *      Author: kelly
+ */
+
+#ifndef COMPARTMENTINTERACTION_H_
+#define COMPARTMENTINTERACTION_H_
+
+class CompartmentInteraction {
+public:
+	CompartmentInteraction();
+	virtual ~CompartmentInteraction();
+};
+
+#endif /* COMPARTMENTINTERACTION_H_ */
