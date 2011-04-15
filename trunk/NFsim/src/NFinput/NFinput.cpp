b3693c5e2a25a05d91e4a4065807016ec07def86
msneddon <msneddon@e73d0046-41a0-3a10-3dbd-efb5796e4b37>
2011-04-15 16:22:10 +0000

    nfsim now allows binding of two sites on the same molecule.  also, scientific notation (such as 6e4) in species block are now accepted (previously scientific notation was only parsed in parameters and rates).

--- trunk/NFsim/src/NFinput/NFinput.cpp
+++ trunk/NFsim/src/NFinput/NFinput.cpp
@@ -632,11 +632,28 @@ bool NFinput::initStartSpecies(
 			try {
 				specCountInteger = NFutil::convertToInt(specCount);
 			} catch (std::runtime_error &e1) {
-				if(parameter.find(specCount)==parameter.end()) {
-					cerr<<"Could not find parameter: "<<specCount<<" when creating species "<<speciesName<<". Quitting"<<endl;
-					return false;
+
+				// if we cannot get it as an integer, try as a double (for instance, for notation
+				// such as 2e4).  We cast it as an int, which will always round the number down
+				// to the nearest whole integer.
+				try {
+					specCountInteger = (int) NFutil::convertToDouble(specCount);
+
+				} catch (std::runtime_error &e1) {
+					// if we cannot get it as an integer, try as a double (for instance, for notation
+					// such as 2e4).  We cast it as an int, which will always round the number down
+					// to the nearest whole integer.
+					try {
+						specCountInteger = (int) NFutil::convertToDouble(specCount);
+					} catch (std::runtime_error &e1) {
+						if(parameter.find(specCount)==parameter.end()) {
+							cerr<<"Could not find parameter: "<<specCount<<" when creating species "<<speciesName<<". Quitting"<<endl;
+							return false;
+						}
+						specCountInteger = (int)parameter.find(specCount)->second;
+					}
+
 				}
-				specCountInteger = (int)parameter.find(specCount)->second;
 			}
 
 			//Make sure we didn't try to create a negative number of molecules
