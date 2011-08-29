f2c471ccc57ec2053bb1203d97e74f303638e8d8
msneddon <msneddon@e73d0046-41a0-3a10-3dbd-efb5796e4b37>
2011-08-29 19:35:26 +0000

    fixed bug in template molecule when clearing molecules after a connected-to syntax search.  In some cases, not all molecules were being cleared, giving rise to situations where adding one observable created dangling match pointers which affected the results of other observables.

--- trunk/NFsim/src/NFcore/moleculeType.cpp
+++ trunk/NFsim/src/NFcore/moleculeType.cpp
@@ -367,6 +367,7 @@ void MoleculeType::removeMoleculeFromRunningSystem(Molecule *&m)
 	}
 
 	m->setAlive(false);
+
 }
 
 
@@ -622,13 +623,13 @@ void MoleculeType::addAllToObservables()
 	/////  WARNING:: when calling this function, be sure to clear all observables
 	/////  first, because this function will not clear observables.
 
-	cout<<"+++++++++ "<<this->getName()<<endl;
+//	cout<<"+++++++++ "<<this->getName()<<endl;
 
 	//Check each observable and see if this molecule should be counted
 	Molecule *mol;  int o=0;  int matches=0;
   	for(molObsIter = molObs.begin(); molObsIter != molObs.end(); molObsIter++)
   	{
-  		cout<<"comparing to obs: "<<(*molObsIter)->getName()<<endl;
+  		//cout<<"comparing to obs: "<<(*molObsIter)->getName()<<endl;
 
   		for( int m=0; m<mList->size(); m++ )
   		{
@@ -636,7 +637,7 @@ void MoleculeType::addAllToObservables()
   			matches = (*molObsIter)->isObservable(mol);
   			(*molObsIter)->add(matches);
   			mol->setIsObs(o,matches);
-  			cout<<"matches:"<<matches<<endl;
+  			//cout<<"matches:"<<matches<<endl;
   		}
   		o++;
 	}
