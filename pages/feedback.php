b86b89e5a6d30c8b5e6368096b4d87d13c477513
msneddon <mwsneddon@lbl.gov>
2015-03-19 15:52:53 -0700

    added website

---
+++ pages/feedback.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+	<title>NFsim - feedback</title>
+
+	<META NAME="description" CONTENT=".">
+	<META NAME="keywords" CONTENT="">
+	<link rel="stylesheet" type="text/css" href="../css/nf_style.css" />
+
+</head>
+
+<body>
+
+
+	<center>
+	<table border=0 cellspacing=0 cellpadding=0>
+	<tr>
+		<!-- <td width=30%></td> -->
+		<td><img src="../images/topLogo.jpg" align=right></td>
+		<td bgcolor=#2E3192 valign="center">
+			<center>
+			<div class="topMenuDiv">
+				<a href="../" class="topMenu">overview</a>&nbsp&nbsp&nbsp
+				<a href="../download" class="topMenu">download</a>&nbsp&nbsp&nbsp
+				<a href="features.html" class="topMenu">features</a> &nbsp&nbsp&nbsp
+				<a href="feedback.html" class="topMenu">feedback</a> <br>
+				<a href="support/support.html" class="topMenu">support</a> &nbsp&nbsp&nbsp
+				<a href="models/models.html" class="topMenu">models</a> &nbsp&nbsp&nbsp
+				<a href="developers.html" class="topMenu">developers</a> &nbsp&nbsp&nbsp
+				<a href="http://emonet.biology.yale.edu" class="topMenu">emonet lab</a>
+			</div>
+			</center>
+		</td>
+		<td bgcolor=#2E3192 width=15%></td>
+		<td><img src="../images/topRight.jpg" align=left></td>
+		<!-- <td bgcolor=#2E3192 width=50%></td> -->
+	</tr>
+	
+	<tr>
+		<td colspan=4>
+		
+		<div class="sectionTitleDiv">
+			feedback</div>
+		</td>
+	</tr>
+	
+	
+	<tr>
+		<td colspan=4>
+		<center>
+		<!--  CONTENT -->
+		
+		
+		<table border=0 width=650><tr><td>
+		
+		<br>
+
+		<p>Please send questions, comments or other requests to:<br>
+		Michael Sneddon (<a href="mailto:mwsneddon@lbl.gov">mwsneddon@lbl.gov</a>)</p>
+
+		<p>Or, you can also open
+		an issue or submit a pull request at:<br> <a href="http://github.com/msneddon/nfsim">http://github.com/msneddon/nfsim</a>.</p>
+
+		<p>We will try to respond as quickly as possible!  Thanks!</p>
+		
+		
+		
+		</td></tr>
+		</table>
+		
+
+		
+		<br><br><br><br><br><br><br><br>
+		
+		
+	</td>
+	</tr>
+
+	
+	
+	
+
+</table>
+</center>
+
+</body>
+</html>
