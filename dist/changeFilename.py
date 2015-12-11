306120f81f69e6ea285797d05ea7635b0c229c00
Jose Juan Tapia <jjtapia@gmail.com>
2015-12-11 10:15:02 -0500

    increasing amount of output information in deployment preparation script

--- dist/changeFilename.py
+++ dist/changeFilename.py
@@ -5,6 +5,7 @@ import os
 version = '1.12'
 destdir = os.path.join(version, '{0}-{1}'.format(platform.system(), platform.architecture()[0]))
 os.makedirs(destdir)
+print 'moving NFSIm to {0}\n'.format(destdir)
 if platform.system() != 'Windows':
     shutil.move('NFsim', os.path.join(destdir, 'NFsim'))
 else:
