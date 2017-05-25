dec53cb28c8c76de85158697ef95ac29c01d126d
Robert Clark <rhclark.pitt@gmail.com>
2017-05-25 19:42:29 -0400

    xx
    xx

---
+++ make_tar.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/perl
+#
+# SYNOPSIS:
+#   make_html.pl [OPTS] 
+#
+# DESCRIPTION:
+#   Create an html page that provides a link to the latest generated
+#   BioNetGen distribution.
+#
+# OPTIONS:
+#   --platform   PLAT  : Choices are Linux, MacOSX, Windows
+
+
+use strict;
+use warnings;
+
+# Perl Core Modules
+use FindBin;
+use File::Spec;
+use Getopt::Long;
+use Cwd ("getcwd");
+use Config;
+use File::Path qw(remove_tree);
+
+
+# distribution version (default undefined)
+my $version = '';
+# platform choices are: (MacOSX or Linux or Windows)
+my $platform = '';
+# path to version file
+my $path_to_version_file = '.';
+
+
+GetOptions( 'help|h'        => sub { display_help(); exit(0); },
+            'platform=s'    => \$platform);
+
+
+print "Platform: $platform\n";
+
+
+my $zip_type  = '';
+my $travis_os = '';
+if ($platform eq "linux") {
+  $zip_type = ".tar.gz";  $travis_os = "Linux";
+  my $ofile_name = "NFsim-source-".$platform.$zip_type;
+
+  print " pwd = \n";
+  system("pwd");
+
+} else {
+  if ($platform eq "osx") {
+  $zip_type = ".tar.gz";  $travis_os = "MacOSX";
+  } else {
+    if ($platform eq "windows") {
+      $zip_type = ".zip";  $travis_os = "Windows";
+    } else {
+      print "Invalid platform: ".$platform."\n";
+      exit;
+    }
+  }
+}
+
+
+exit;
+
+
+# ########################################################################
+#   HELP 
+# ########################################################################
+# display help menu
+sub display_help
+{
+print <<END_HELP
+make_html.pl
+SYNOPSIS:
+   make_html.pl [OPTS] 
+DESCRIPTION:
+   Create an html page that provides a link to the latest generated
+   BioNetGen distribution.
+OPTIONS:
+   --platform   PLAT  : Choices are Linux, MacOSX, Windows
+END_HELP
+
+}
