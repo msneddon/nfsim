26556d1af0c00a4480550fc2789e4cce79188627
Robert Clark <rhclark.pitt@gmail.com>
2017-07-14 18:40:54 -0400

    Copy NFsim to NFsim-Win32 or NFsim-Win64.
    Copy NFsim to NFsim-Win32 or NFsim-Win64.

--- .make_tar.pl
+++ .make_tar.pl
@@ -60,7 +60,6 @@ if ($platform eq "linux") {
   
     if ($platform eq "Win32") {
       $zip_type = ".zip";  $travis_os = "Win32";
-      system('copy  .\build\NFsim.exe  .\build\NFsim-Win32.exe');
       my $archive_file = "build/NFsim-source-Win32".$zip_type;
       print "\nCreating NFsim".$platform.".exe source archive:\n";
       system("7z a  ${archive_file} doc models src test tools validate CMakeLists.txt LICENSE.txt README.txt makefile NFsim_manual_v1.12.pdf ");
@@ -68,7 +67,6 @@ if ($platform eq "linux") {
     } else {
       if ($platform eq "Win64") {
         $zip_type = ".zip";  $travis_os = "Win64";
-        system('copy  .\build\NFsim.exe  .\build\NFsim-Win64.exe');
         my $archive_file = "./build/NFsim-source-Win64".$zip_type;
         print "\nCreating NFsim".$platform.".exe source archive:\n";
         system("7z a  ${archive_file} doc models src test tools validate CMakeLists.txt LICENSE.txt README.txt makefile NFsim_manual_v1.12.pdf ");
